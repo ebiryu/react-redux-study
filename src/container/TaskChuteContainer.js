c7a88fad24773c341b8ab975bfb33280ecf2e6d3
ebiryu <ebiryu.zatta@gmail.com>
2018-05-25 15:28:32 +0900

    add container

---
+++ src/container/TaskChuteContainer.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux';
+
+// import * as actions from '../actions';
+// import '../css/main.css';
+
+class TaskChuteContainer extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+// const mapState = (state, ownProps) => ({
+//   calculator: state.calculator,
+// })
+
+// const mapDispatch = (dispatch) => ({
+//   actions: bindActionCreators(actions, dispatch),
+// })
+
+// export default connect(mapState,mapDispatch)(CalculatorContainer);
+export default TaskChuteContainer;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import TaskChuteContainer from './container/TaskChuteContainer';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<TaskChuteContainer />, document.getElementById('root'));
 registerServiceWorker();
