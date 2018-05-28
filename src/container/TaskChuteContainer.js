52cbdf8f6681fb2fd156086ed274ba6d05e9c108
ebiryu <ebiryu.zatta@gmail.com>
2018-05-28 12:08:03 +0900

    apply redux

--- src/container/TaskChuteContainer.js
+++ src/container/TaskChuteContainer.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-// import { connect } from 'react-redux';
-// import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-// import * as actions from '../actions';
+import * as actions from '../actions';
 import '../css/main.css';
 
 class TaskChuteContainer extends Component {
@@ -24,13 +24,12 @@ class TaskChuteContainer extends Component {
   }
 }
 
-// const mapState = (state, ownProps) => ({
-//   calculator: state.calculator,
-// })
+const mapState = (state, ownProps) => ({
+  ...state
+})
 
-// const mapDispatch = (dispatch) => ({
-//   actions: bindActionCreators(actions, dispatch),
-// })
+const mapDispatch = (dispatch) => ({
+  actions: bindActionCreators(actions, dispatch),
+})
 
-// export default connect(mapState,mapDispatch)(CalculatorContainer);
-export default TaskChuteContainer;
+export default connect(mapState,mapDispatch)(TaskChuteContainer);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import TaskChuteContainer from './container/TaskChuteContainer';
+import reducer from './reducers'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<TaskChuteContainer />, document.getElementById('root'));
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <TaskChuteContainer />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
