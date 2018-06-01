8323fe41227d34d079d65d980a75ae76b9716220
ebiryu <ebiryu.zatta@gmail.com>
2018-06-01 13:44:22 +0900

    update columnTitle, newTaskInput

--- src/components/Column.js
+++ src/components/Column.js
@@ -3,40 +3,23 @@ import PropTypes from 'prop-types';
 import ColumnItem from '../components/ColumnItem';
 
 class Column extends React.Component {
-  constructor(props) {
-    super(props);
-    this.focused = React.createRef();
-  }
-  componentDidUpdate() {
-    if (this.focused.current) {
-      this.focused.current.focus();
-    }
-  }
   render() {
     const {columnNumber, columnState, actions} = this.props;
-    const submitTask = (e, columnNumber, inputValue) => {
-      e.preventDefault();
-      actions.onSubmitTask(columnNumber, inputValue);
-    };
     return (
       <div className="column">
-        { columnState.isTitleEditable 
-          ? <form onSubmit={() => actions.onClickColumnTitle(columnNumber)}>
-            <input className="column__title"
-              onBlur={() => actions.onClickColumnTitle(columnNumber)}
-              onChange={event => actions.editColumnTitle(columnNumber, event.target.value)}
-              value={columnState.columnTitle}
-              ref={this.focused}
-            />
-          </form>
-          : <span className="column__title"
-            onClick={() => actions.onClickColumnTitle(columnNumber)}
-          >{columnState.columnTitle}</span>
-        }
-        <form className="column__form" onSubmit={() => submitTask(columnNumber, columnState.inputValue)}>
-          <input className="column__input" value={columnState.inputValue} onChange={event => actions.onInputTask(columnNumber, event.target.value)} />
-          <button className="column__submit-button" type="submit">+</button>
-        </form>
+        <ColumnTitle
+          columnTitle={columnState.columnTitle}
+          isTitleEditable={columnState.isTitleEditable}
+          columnNumber={columnNumber}
+          onClickColumnTitle={actions.onClickColumnTitle}
+          editColumnTitle={actions.editColumnTitle}
+        />
+        <NewTaskInput
+          columnNumber={columnNumber}
+          inputValue={columnState.inputValue}
+          onInputTask={actions.onInputTask}
+          onSubmitTask={actions.onSubmitTask}
+        />
         <ul className="column__list">
           { columnState.tasks.map( (task, index) => {
             return (
@@ -60,10 +43,68 @@ class Column extends React.Component {
   }
 }
 
+class ColumnTitle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.focused = React.createRef();
+  }
+  componentDidUpdate() {
+    if (this.focused.current) {
+      this.focused.current.focus();
+    }
+  }
+  render() {
+    const {isTitleEditable, columnNumber, onClickColumnTitle, editColumnTitle, columnTitle} = this.props;
+    return isTitleEditable 
+      ? 
+      <form onSubmit={() => onClickColumnTitle(columnNumber)}>
+        <input className="column__title"
+          onBlur={() => onClickColumnTitle(columnNumber)}
+          onChange={event => editColumnTitle(columnNumber, event.target.value)}
+          value={columnTitle}
+          ref={this.focused}
+        />
+      </form>
+      : 
+      <span className="column__title" onClick={() => onClickColumnTitle(columnNumber)}>
+        {columnTitle}
+      </span>
+      
+  }
+}
+
+const NewTaskInput = ({columnNumber, inputValue, onInputTask, onSubmitTask}) => {
+  const submitTask = (e, columnNumber, inputValue) => {
+    e.preventDefault();
+    onSubmitTask(columnNumber, inputValue);
+  };
+  return (
+    <form className="column__form" onSubmit={() => submitTask(columnNumber, inputValue)}>
+      <input className="column__input" value={inputValue} onChange={event => onInputTask(columnNumber, event.target.value)} />
+      <button className="column__submit-button" type="submit">+</button>
+    </form>
+  )
+}
+
 Column.propTypes = {
   columnNumber: PropTypes.number.isRequired,
   columnState: PropTypes.object.isRequired,
   actions: PropTypes.object.isRequired,
+};
+
+ColumnTitle.propTypes = {
+  isTitleEditable: PropTypes.bool.isRequired,
+  columnNumber: PropTypes.number.isRequired,
+  onClickColumnTitle: PropTypes.func.isRequired,
+  editColumnTitle: PropTypes.func.isRequired,
+  columnTitle: PropTypes.string.isRequired,
+};
+
+NewTaskInput.propTypes = {
+  columnNumber: PropTypes.number.isRequired,
+  inputValue: PropTypes.string.isRequired,
+  onInputTask: PropTypes.func.isRequired,
+  onSubmitTask: PropTypes.func.isRequired,
 };
 
 export default Column;
