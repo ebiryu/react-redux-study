532d1eff26149ef5fed063b9e281ea8ffe7246d2
ebiryu <ebiryu.zatta@gmail.com>
2018-06-14 15:38:41 +0900

    npm i -D eslint-plugin-jest

--- src/components/test/ColumnTitle.test.js
+++ src/components/test/ColumnTitle.test.js
@@ -14,13 +14,13 @@ describe('Column Title ', () => {
 
   it('子コンポーネントが存在すること(not editable)', () => {
     const columnTitle = shallow(<ColumnTitle isTitleEditable={false}/>)
-    expect(columnTitle.find('span').length).toBe(1)
-    expect(columnTitle.find('form').length).toBe(0)
+    expect(columnTitle.find('span')).toHaveLength(1)
+    expect(columnTitle.find('form')).toHaveLength(0)
   })
   it('子コンポーネントが存在すること(editable)', () => {
     const columnTitle = shallow(<ColumnTitle isTitleEditable={true}/>)
-    expect(columnTitle.find('span').length).toBe(0)
-    expect(columnTitle.find('form').length).toBe(1)
+    expect(columnTitle.find('span')).toHaveLength(0)
+    expect(columnTitle.find('form')).toHaveLength(1)
   })
   it('初期値 (expected "title")', () => {
     const columnTitle = shallow(<ColumnTitle columnTitle={'title'}/>)
@@ -31,27 +31,27 @@ describe('Column Title ', () => {
     columnTitle.find('span').simulate('click')
     // クリックしても'span'のまま。reduxには影響しない模様。
     expect(columnTitle.find('span').exists()).toBe(true)
-    expect(testFn).toHaveBeenCalled
+    expect(testFn).toHaveBeenCalled()
   })
   it('submit時', () => {
     const columnTitle = shallow(<ColumnTitle isTitleEditable={true} onClickColumnTitle={testFn}/>)
     columnTitle.find('form').simulate('submit')
-    expect(testFn).toHaveBeenCalled
+    expect(testFn).toHaveBeenCalled()
   })
   it('blur時', () => {
     const columnTitle = shallow(<ColumnTitle isTitleEditable={true} onClickColumnTitle={testFn}/>)
     columnTitle.find('input').simulate('blur')
-    expect(testFn).toHaveBeenCalled
+    expect(testFn).toHaveBeenCalled()
   })
   it('titleを入力時', () => {
     const columnTitle = shallow(<ColumnTitle isTitleEditable={true} editColumnTitle={testFn}/>)
     columnTitle.find('input').simulate('change', {target: {value: '7'}})
-    expect(testFn).toHaveBeenCalled
+    expect(testFn).toHaveBeenCalled()
   })
   it('autofocus', () => {
     const columnTitle = mount(<ColumnTitle isTitleEditable={true}/>)
     const { focused } = columnTitle.instance()
-    const spy = jest.spyOn(focused.current, 'focus')
+    jest.spyOn(focused.current, 'focus')
 
     columnTitle.instance().componentDidUpdate()
     expect(focused.current.focus).toHaveBeenCalledTimes(1)
